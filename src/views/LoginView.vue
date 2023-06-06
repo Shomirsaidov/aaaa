38c5984d3fd734800aeabbf2310a903240be60cf
Shomirsaidov <115943155+Shomirsaidov@users.noreply.github.com>
2023-06-06 12:38:12 +0300

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -19,7 +19,7 @@
         <div class="card p-4 px-8 md:px-32 md:mt-16 border-2 rounded-md shadow-xl space-y-4 w-full" v-if="loginScreen">
             <h1 class="text-xl font-bold">Вход</h1>
             <input type="text" placeholder="ФИО" class="p-2 border-2 rounded-lg text-lg outline-none w-full"><br/>
-            <input type="text" placeholder="Пароль" class="p-2 border-2 rounded-lg text-lg outline-none w-full">
+            <input type="password" placeholder="Пароль" class="p-2 border-2 rounded-lg text-lg outline-none w-full">
             <div class="flex justify-end">
                 <button class="bg-blue-600 text-white p-2 rounded-lg mt-2 text-lg w-full">Войти</button>
             </div>
