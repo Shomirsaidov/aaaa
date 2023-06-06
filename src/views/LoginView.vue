791edb6ece1c11f763dd87fedc0f7da8ebccb69c
Shomirsaidov <115943155+Shomirsaidov@users.noreply.github.com>
2023-06-06 12:37:44 +0300

    Update LoginView.vue

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,12 +1,12 @@
 <template class="flex space-x-4 items-center">
     
     <div class="flex justify-center">
-        <div class="card p-4 px-8 md:px-32 md:mt-16 border-2 rounded-md shadow-xl space-y-4 w-full " v-if="!loginScreen">
+        <div class="card p-4 px-8 md:px-32 md:mt-16 border-2 rounded-md shadow-xl space-y-4 w-full" v-if="!loginScreen">
             <h1 class="text-xl font-bold">Регистрация</h1>
-            <input type="text" placeholder="ФИО" class="p-2 border-2 rounded-lg text-lg outline-none"><br/>
-            <input type="text" placeholder="Номер телефона" class="p-2 border-2 rounded-lg text-lg outline-none"><br/>
-            <input type="text" placeholder="Электронная почта" class="p-2 border-2 rounded-lg text-lg outline-none"><br/>
-            <input type="password" placeholder="Придумайте пароль" class="p-2 border-2 rounded-lg text-lg outline-none">
+            <input type="text" placeholder="ФИО" class="p-2 border-2 rounded-lg text-lg outline-none w-full"><br/>
+            <input type="text" placeholder="Номер телефона" class="p-2 border-2 rounded-lg text-lg outline-none w-full"><br/>
+            <input type="text" placeholder="Электронная почта" class="p-2 border-2 rounded-lg text-lg outline-none w-full"><br/>
+            <input type="password" placeholder="Придумайте пароль" class="p-2 border-2 rounded-lg text-lg outline-none w-full">
             <div class="flex justify-end">
                 <button class="bg-blue-600 text-white p-2 rounded-lg mt-2 text-lg w-full">Далее</button>
             </div>
@@ -16,10 +16,10 @@
             </div>
         </div>
 
-        <div class="card p-4 px-32 mt-16 border-2 rounded-md shadow-xl space-y-4" v-if="loginScreen">
+        <div class="card p-4 px-8 md:px-32 md:mt-16 border-2 rounded-md shadow-xl space-y-4 w-full" v-if="loginScreen">
             <h1 class="text-xl font-bold">Вход</h1>
-            <input type="text" placeholder="ФИО" class="p-2 border-2 rounded-lg text-lg outline-none"><br/>
-            <input type="text" placeholder="Пароль" class="p-2 border-2 rounded-lg text-lg outline-none">
+            <input type="text" placeholder="ФИО" class="p-2 border-2 rounded-lg text-lg outline-none w-full"><br/>
+            <input type="text" placeholder="Пароль" class="p-2 border-2 rounded-lg text-lg outline-none w-full">
             <div class="flex justify-end">
                 <button class="bg-blue-600 text-white p-2 rounded-lg mt-2 text-lg w-full">Войти</button>
             </div>
